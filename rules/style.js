7a6805de51f97c6ff2a2ff81dd18274e989aead9
Jerel Miller <jerelmiller@gmail.com>
2016-03-13 20:00:24 -0600

    Finish adding style rules

--- rules/style.js
+++ rules/style.js
@@ -25,16 +25,20 @@ module.exports = {
     'no-lonely-if': 2,
     'no-mixed-spaces-and-tabs': 2,
     'no-multiple-empty-lines': [2, { 'max': 2, 'maxEOF': 1, 'maxBOF': 0 }],
+    'no-negated-condition': 2,
     'no-nested-ternary': 2,
     'no-new-object': 2,
     'no-spaced-func': 2,
     'no-trailing-spaces': 2,
     'no-unneeded-ternary': 2,
+    'no-whitespace-before-property': 2,
     'object-curly-spacing': [2, 'always', {
       'arraysInObjects': false,
       'objectsInObjects': false
     }],
     'one-var': [2, 'never'],
+    'operator-assignment': [2, 'always'],
+    'operator-linebreak': [2, 'after'],
     'padded-blocks': [2, 'never'],
     'quote-props': [2, 'as-needed', {
       'keywords': false,
@@ -43,9 +47,14 @@ module.exports = {
     }],
     'quotes': [2, 'single', 'avoid-escape'],
     'semi': [2, 'never'],
+    'sort-imports': [2, {
+      'memberSyntaxSortOrder': ['none', 'single', 'multiple', 'all']
+    }],
     'space-before-blocks': [2, 'always'],
+    'space-before-function-paren': [2, 'never'],
     'space-infix-ops': 2,
     'space-in-parens': [2, 'never'],
+    'space-unary-ops': [2, { 'words': true, 'nonwords': false }],
     'spaced-comment': [2, 'always']
   }
 }
