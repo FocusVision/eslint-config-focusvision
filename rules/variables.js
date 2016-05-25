ffe5d7d22bc5e01b09790a00a0800f907e94ef44
Jerel Miller <jerelmiller@gmail.com>
2016-05-24 23:13:27 -0600

    Allow the use of undefined as a variable. Allow shadowing of outer variable names. Allow functions to be defined after they are used

--- rules/variables.js
+++ rules/variables.js
@@ -2,12 +2,9 @@ module.exports = {
   'rules': {
     'no-delete-var': 2,
     'no-label-var': 2,
-    'no-shadow': 2,
     'no-shadow-restricted-names': 2,
     'no-undef': 2,
     'no-undef-init': 2,
-    'no-undefined': 2,
-    'no-unused-vars': [2, { 'args': 'after-used', 'vars': 'all' }],
-    'no-use-before-define': 2
+    'no-unused-vars': [2, { 'args': 'after-used', 'vars': 'all' }]
   }
 }
